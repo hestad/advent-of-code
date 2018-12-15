f21f5ae48ef4321fd0ee3f564300d6a9a1b6050a
John Andre Hestad <john.hestad@blank.no>
2018-12-15 17:44:11 +0100

    Add .gitignore, README python requirement and setup.py

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# advent-of-code
-Solutions for https://adventofcode.com
+# Advent Of Code
+Solutions to https://adventofcode.com
+
+## Python development environment
+* Install Python >= 3.7 (e.g. https://github.com/pyenv/pyenv-installer)
+* Install pip (pyenv includes this)
+* `pip install -e .`
---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="advent-of-code",
+    version="0.0.1",
+    author="John André Hestad",
+    author_email="jahestad@gmail.com",
+    description="Solutions to https://adventofcode.com",
+    url="https://github.com/hestad/advent-of-code",
+    install_requires=['numpy>=1.15.4'],
+    python_requires='>=3.7',
+    classifiers=(
+        "Programming Language :: Python :: 3"
+    ),
+)
